3d0e151d8d27897262573dd08f88fd2f3b2d7d79
Steve Van Opstal <stevevanopstal@gmail.com>
2018-05-11 12:20:14 +0200

    chore: add public_api

---
+++ public_api.ts
@@ -0,0 +1,3 @@
+export {CacheInterceptorModule, MAX_AGE_MS} from './src/cache.module';
+export {EnsureHttpsInterceptorModule} from './src/ensure-https.module';
+export {PREFIX, PrefixUrlInterceptorModule} from './src/prefix-url.module';
