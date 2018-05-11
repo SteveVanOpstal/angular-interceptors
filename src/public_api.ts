979e5608b2226b1302da8d23f38ca3cfc0f96798
Steve Van Opstal <stevevanopstal@gmail.com>
2018-05-11 12:12:33 +0200

    build(lint): fix linter errors

--- src/public_api.ts
+++
@@ -1,3 +0,0 @@
-export {CacheInterceptorModule, MAX_AGE_MS} from './cache.module';
-export {EnsureHttpsInterceptorModule} from './ensure-https.module';
-export {PREFIX, PrefixUrlInterceptorModule} from './prefix-url.module';
